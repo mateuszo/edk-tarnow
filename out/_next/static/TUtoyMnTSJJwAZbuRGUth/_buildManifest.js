5ae832085dd761c0d16a657c6266c55c5a01514d
Mateusz Ostafil <ratwolf0@gmail.com>
2023-03-28 23:33:54 +0200

    Deploy

--- out/_next/static/TUtoyMnTSJJwAZbuRGUth/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/188-bcb8194d34a56d21.js","static/chunks/pages/index-1f8dec94f8a416c6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
